dfb86aaf8c03548599ffdd1e86caec1304ca8c77
9EED <88392191+9EED@users.noreply.github.com>
2024-03-16 19:27:54 +0100

    error catching

--- src/routes/helper.js
+++ src/routes/helper.js
@@ -1,5 +1,10 @@
 export async function load(link){
-    let response = await fetch(link);
-    let data = await response.json();
-    return data;
+    try{
+        let response = await fetch(link);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 }
