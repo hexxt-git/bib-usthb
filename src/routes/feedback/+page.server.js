5c44d145dd43ccb18b4288927922de026a10f282
9EED <88392191+9EED@users.noreply.github.com>
2024-04-02 19:00:08 +0100

    trying to debug

--- src/routes/feedback/+page.server.js
+++ src/routes/feedback/+page.server.js
@@ -9,21 +9,21 @@ if(resend_key.slice(0, 6) === 're_GUz'){
     resend = new Resend( resend_key );
     actions = {
        default: async({ request }) =>{ // it comes in a weird data struct and i convert it to an object
-           const formData = [...await request.formData()].reduce((acc, arr)=>{
-               acc[arr[0]] = arr[1]
-               return acc
-           },{})
+            const formData = [...await request.formData()].reduce((acc, arr)=>{
+                acc[arr[0]] = arr[1]
+                return acc
+            },{})
     
-           console.log(formData)
-    
-           const subject = `bib-usthb.com ${formData.topic} from ${formData.name || 'anonymous'}`
-           const html = `
-               <h3>${formData.topic}</h3>
-               <p>${formData.detail}</p>
-               <hr>
-               this feedback mail was sent on ${new Date()}
-               by ${formData.name} ${formData.email} ${formData.usthb_student == 'on' ? 'a' : 'not a'} usthb student. studying ${formData.domain}
+           
+            const subject = `bib-usthb.com ${formData.topic} from ${formData.name || 'anonymous'}`
+            const html = `
+                <h3>${formData.topic}</h3>
+                <p>${formData.detail}</p>
+                <hr>
+                this feedback mail was sent on ${new Date()}
+                by ${formData.name} ${formData.email} ${formData.usthb_student == 'on' ? 'a' : 'not a'} usthb student. studying ${formData.domain}
            `
+           console.log({resend_key, formData, subject, html})
     
            resend.emails.send({
                from: 'bibusthb@resend.dev',
