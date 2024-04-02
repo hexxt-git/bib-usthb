71406b19abe0667e65efc70568e6e7467a7d8a56
9EED <88392191+9EED@users.noreply.github.com>
2024-04-02 18:45:17 +0100

    resend for feedback

---
+++ src/routes/feedback/+page.server.js
@@ -0,0 +1,37 @@
+import { Resend } from "resend"
+import { resend_key } from '$env/static/private'
+
+
+let resend;
+export let actions = {default: async() => { console.error('resend api key not found') }};
+
+if(resend_key.slice(0, 6) === 're_GUz'){
+    resend = new Resend( resend_key );
+    actions = {
+       default: async({ request }) =>{ // it comes in a weird data struct and i convert it to an object
+           const formData = [...await request.formData()].reduce((acc, arr)=>{
+               acc[arr[0]] = arr[1]
+               return acc
+           },{})
+    
+           console.log(formData)
+    
+           const subject = `bib-usthb.com ${formData.topic} from ${formData.name || 'anonymous'}`
+           const html = `
+               <h3>${formData.topic}</h3>
+               <p>${formData.detail}</p>
+               <hr>
+               this feedback mail was sent on ${new Date()}
+               by ${formData.name} ${formData.email} ${formData.usthb_student == 'on' ? 'a' : 'not a'} usthb student. studying ${formData.domain}
+           `
+    
+           resend.emails.send({
+               from: 'bibusthb@resend.dev',
+               to: ['bibusthb@gmail.com'],
+               subject: subject,
+               html: html
+           })
+    
+       }
+    }
+}
