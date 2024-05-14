39d3ab0957235a093b2dc3bade8ac1399a19f40a
HEXXT <88392191+9EED@users.noreply.github.com>
2024-05-14 02:37:39 +0100

    Update theme_store.js

--- src/components/theme_store.js
+++ src/components/theme_store.js
@@ -1,20 +1,22 @@
 import { writable } from "svelte/store"
 
 let themes = ['light', 'dark', 'ocean', 'fire', 'bubble', 'sun']
-export let theme_store = writable('ocean')
 
+export let theme_store = writable('ocean')
 export let next_theme = writable('');
+
+const get_next = (theme) => themes[(themes.indexOf(theme) + 1) % themes.length]
+
+theme_store.subscribe((theme)=>{
+    next_theme.set(get_next(theme))
+})
 
 if(typeof window !== 'undefined'){
     theme_store.set(localStorage.getItem('theme') || 'ocean')
-    next_theme.set(themes[(themes.indexOf(localStorage.getItem('theme'))+1) % themes.length] || 'fire')
 }
 
 export let switch_theme = () => {
-    next_theme.update((theme) => {
-        let new_theme = themes[(themes.indexOf(theme) + 1) % themes.length]
-        localStorage.setItem('theme', new_theme)
-        theme_store.set(theme)
-        return new_theme
+    theme_store.update((theme) => {
+        return get_next(theme)
     })
 }
